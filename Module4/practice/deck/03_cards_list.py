cdb4211010925cd73e8faf7950dc50009a697751
Nikoaly <74960807+Nikoaly@users.noreply.github.com>
2023-04-19 22:10:46 +0300

    Update 03_cards_list.py

--- Module4/practice/deck/03_cards_list.py
+++ Module4/practice/deck/03_cards_list.py
@@ -1,21 +1,41 @@
 class Card:
-    # TODO-0: сюда копируем реализацию класса карты из предыдущего задания
-    ...
-
+     def __init__(self, value, suit):
+        self.value = value  # Значение карты(2, 3... 10, J, Q, K, A)
+        self.suit = suit  # Масть карты
+    def to_str(self):
+        # Символы для отображения мастей
+        suit_symbols = {"Hearts": "\u2665", "Diamonds": "\u2666",
+                        "Clubs": "\u2663", "Spades": "\u2660"}
+        # Строковое представление карты вида "значение масть"
+        return self.value + suit_symbols[self.suit]
+    def equal_suit(self, other_card):
+        return self.suit == other_card.suit
 
 values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 hearts_cards = []
-# TODO-1: добавьте в список hearts_cards все червовые карты(от 2-ки до туза)
-
+for value in values:
+    card = Card(value, "Hearts")
+    hearts_cards.append(card)
+    
 diamonds_cards = []
-# TODO-2: добавьте в список diamonds_cards все бубновые карты(от туза до 2-ки)
+for value in reversed(values):
+    card = Card(value, "Diamonds")
 
-# TODO-3: выведите все карты из списка hearts_cards в терминал через запятую в одну строку:
-# Пример вывода: 2♥, 3♥, 4♥ ... A♥
-
+cards_str = ", ".join([card.to_str() for card in hearts_cards])
+print(cards_str)
 
 cards = [
     Card("2", "Hearts"),
     Card("3", "Hearts"),
     Card("4", "Hearts"),
-    ...]
+    Card("5", "Hearts"),
+    Card("6", "Hearts"),
+    Card("7", "Hearts"),
+    Card("8", "Hearts"),
+    Card("9", "Hearts"),
+    Card("10", "Hearts"),
+    Card("J", "Hearts"),
+    Card("Q", "Hearts"),
+    Card("K", "Hearts"),
+    Card("A", "Hearts"),
+]
