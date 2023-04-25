7b1467df67353acedd84d8ba34a7db7cf02a82d3
Nikoaly <74960807+Nikoaly@users.noreply.github.com>
2023-04-25 20:40:56 +0300

    Update maze1.py

--- Module7/practice/01_Maze-1/maze1.py
+++ Module7/practice/01_Maze-1/maze1.py
@@ -2,7 +2,30 @@
 
 # Опишите список смежности по изображению лабиринта из файла task.md
 graph = [
-    ...
+    # список смежности
+    [1],             # 0
+    [0, 4],          # 1
+    [5],             # 2
+    [4, 6],          # 3
+    [1, 3, 5],       # 4
+    [2, 4],         # 5
+    [3],            # 6
+    [8],             # 7
+    [7],             # 8
 ]
+def can_reach_bank(graph, home, bank):
+    visited = set()
+    stack = [home]
+    while stack:
+        node = stack.pop()
+        if node in visited:
+            continue
+        visited.add(node)
+        if node == bank[0] or node == bank[1]:
+            return "Можно дойти до банка"
+        stack.extend(graph[node])
+        return "Невозможно дойти до банка"
+
+print(can_reach_bank(graph, 0, [7, 8]))
 
 # Решите задачу и выведите ответ в нужном формате
